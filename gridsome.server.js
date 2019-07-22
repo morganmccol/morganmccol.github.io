adae29efebf3fb4b18a8a7cd0dbb6de8dbe7dde8
Morgan McCollum <morgan.mccol@gmail.com>
2019-07-21 22:30:12 -0400

    add gridsome base theme

--- gridsome.server.js
+++ gridsome.server.js
@@ -2,15 +2,11 @@
 // on server-side and add custom data to the GraphQL data layer.
 // Learn more: https://gridsome.org/docs/server-api
 
-// Changes here require a server restart.
+// Changes here requires a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = function (api) {
-  api.loadSource(({ addContentType }) => {
-    // Use the Data Store API here: https://gridsome.org/docs/data-store-api
-  })
-
-  api.createPages(({ createPage }) => {
-    // Use the Pages API here: https://gridsome.org/docs/pages-api
+  api.loadSource(store => {
+    // Use the Data store API here: https://gridsome.org/docs/data-store-api
   })
 }
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,55 @@
 <template>
-  <Layout>
-    
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-    
-    <h1>Hello, world!</h1>
-   
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
+  <Layout :show-logo="false">
+    <!-- Author intro -->
+    <Author :show-title="true" />
 
-    <p class="home-links">
-      <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
+    <!-- List posts -->
+    <div class="posts">
+      <PostCard v-for="edge in $page.posts.edges" :key="edge.node.id" :post="edge.node"/>
+    </div>
 
   </Layout>
 </template>
 
+<page-query>
+{
+  posts: allPost(filter: { published: { eq: true }}) {
+    edges {
+      node {
+        id
+        title
+        path
+        tags {
+          id
+          title
+          path
+        }
+        date (format: "D. MMMM YYYY")
+        timeToRead
+        description
+        coverImage (width: 770, height: 380, blur: 10)
+        ...on Post {
+            id
+            title
+            path
+        }
+      }
+    }
+  }
+}
+</page-query>
+
 <script>
+import Author from '~/components/Author.vue'
+import PostCard from '~/components/PostCard.vue'
+
 export default {
+  components: {
+    Author,
+    PostCard
+  },
   metaInfo: {
     title: 'Hello, world!'
   }
 }
 </script>
-
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
